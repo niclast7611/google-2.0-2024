929c055847ee481029c1ced1bc310c80b083e42a
Nic Last <nic@ripemetrics.com>
2024-03-29 17:18:16 -0700

    Add @badrap/bar-of-progress package and implement progress bar in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,21 @@ import Head from "next/head";
 import Layout from "../app/layout";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import ProgressBar from "@badrap/bar-of-progress";
+import { Router } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const progress = new ProgressBar({
+    size: 4,
+    color: "#1d4ed8",
+    className: "z-50",
+    delay: 100,
+  });
+
+  Router.events.on("routeChangeStart", progress.start);
+  Router.events.on("routeChangeComplete", progress.finish);
+  Router.events.on("routeChangeError", progress.finish);
+
   return (
     <Layout>
       <Head>
